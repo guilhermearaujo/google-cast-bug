7811c8897936a60fd233608804cfa186023468c5
Guilherme Araújo <guilhermeama@gmail.com>
2018-01-19 16:04:13 -0200

    Add bug

--- Cast/AppDelegate.swift
+++ Cast/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleCast
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,32 +16,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    let discoveryCriteria = GCKDiscoveryCriteria(applicationID: "INSERT_APP_ID")
+    let options = GCKCastOptions(discoveryCriteria: discoveryCriteria)
+
+    GCKCastContext.setSharedInstanceWith(options)
+    GCKLogger.sharedInstance().delegate = self
+    GCKCastContext.sharedInstance().useDefaultExpandedMediaControls = true
+
+    let vc = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()!
+    let castContainerVC = GCKCastContext.sharedInstance().createCastContainerController(for: vc)
+    castContainerVC.miniMediaControlsItemEnabled = true
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = castContainerVC
+    window?.makeKeyAndVisible()
+
+    let expandedControlsVC = GCKCastContext.sharedInstance().defaultExpandedMediaControlsViewController
+    expandedControlsVC.setButtonType(.skipPrevious, at: 0)
+    expandedControlsVC.setButtonType(.rewind30Seconds, at: 1)
+    expandedControlsVC.setButtonType(.forward30Seconds, at: 2)
+    expandedControlsVC.setButtonType(.skipNext, at: 3)
+
     return true
   }
-
-  func applicationWillResignActive(_ application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-  }
-
-  func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-  }
-
-  func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-  }
-
-  func applicationDidBecomeActive(_ application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
-
-  func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-  }
-
-
 }
 
+extension AppDelegate: GCKLoggerDelegate {
+  func logMessage(_ message: String, at level: GCKLoggerLevel, fromFunction function: String, location: String) {
+    #if DEBUG
+      NSLog("%@  %@", function, message)
+    #endif
+  }
+}
--- Cast/ViewController.swift
+++ Cast/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import GoogleCast
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    let button = GCKUICastButton(frame: CGRect(x: 20, y: 100, width: 40, height: 40))
+    button.backgroundColor = .red
+    view.addSubview(button)
+
+    let enqueueButton = UIButton(frame: CGRect(x: 20, y: 200, width: 80, height: 40))
+    enqueueButton.addTarget(self, action: #selector(enqueue), for: .touchUpInside)
+    enqueueButton.setTitle("Enqueue!", for: .normal)
+    enqueueButton.backgroundColor = .red
+    view.addSubview(enqueueButton)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @objc func enqueue() {
+    let queue: [GCKMediaQueueItem] = [newItem(), newItem(), newItem(), newItem()]
+    GCKCastContext.sharedInstance()
+      .sessionManager
+      .currentSession?
+      .remoteMediaClient?
+      .queueLoad(queue, start: 0, repeatMode: .off)
   }
 
+  func newItem() -> GCKMediaQueueItem {
+    return GCKMediaQueueItem(
+      mediaInformation: newMediaInfo(),
+      autoplay: true,
+      startTime: 0,
+      playbackDuration: 0,
+      preloadTime: 0,
+      activeTrackIDs: nil,
+      customData: nil
+    )
+  }
 
+  func newMediaInfo() -> GCKMediaInformation {
+    return GCKMediaInformation(
+      contentID: "http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8",
+      streamType: .buffered,
+      contentType: "",
+      metadata: nil,
+      streamDuration: 0,
+      mediaTracks: nil,
+      textTrackStyle: nil,
+      customData: nil
+    )
+  }
 }
 
